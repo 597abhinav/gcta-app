55a60ef6d3899952992d905680b4608d54470927
597abhinav <597abhi.mathur@gmail.com>
2018-07-09 19:38:42 +0530

    Added connection

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const path = require('path');
+const socketIO = require('socket.io');
+const http = require('http');
 
 var app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname + './../public')));
+var server = http.createServer(app);
+var io = socketIO(server);
 
+io.on('connection', function(socket) {
+  console.log("New User Connected");
+
+  socket.on('disconnect', function() {
+    console.log("User Disconnected");
+  });
+})
 // app.get('/', (req, res) => {
 //   res.sendFile(path.join(__dirname, './../public', 'index.html'));
 // });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
